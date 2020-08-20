0d3d643d79993cdad2e994e7ef182717e9212f4f
kseggelke918 <katie.seggelke@gmail.com>
2020-08-20 15:36:09 -0600

    add model validations for user and player models

--- le-pool-backend/app/models/player.rb
+++ le-pool-backend/app/models/player.rb
@@ -1,3 +1,4 @@
 class Player < ApplicationRecord
     belongs_to :game
+    validates :name, presence: true 
 end
--- le-pool-backend/app/models/user.rb
+++ le-pool-backend/app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
     has_many :games
+    validates :name, :email, presence: true 
 end
