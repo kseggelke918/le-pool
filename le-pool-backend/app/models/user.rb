cf426d980998cd3cc2777e430901e7960f2b31a1
kseggelke918 <katie.seggelke@gmail.com>
2020-08-20 15:42:02 -0600

    add seed data

--- le-pool-backend/app/models/user.rb
+++ le-pool-backend/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     has_many :games
     validates :name, :email, presence: true 
+    validates :email, uniqueness: true 
 end
--- le-pool-backend/db/seeds.rb
+++ le-pool-backend/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(name: 'Katie', email: 'katie@test.com')
+
+game = Game.create(date: Time.now, game_name: 'seeded_game')
+
+player1 = Player.create(name: 'player1')
+player2 = Player.create(name: 'player2')
