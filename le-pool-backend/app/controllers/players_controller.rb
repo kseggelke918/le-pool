b78cdff64adacd90cf34094495f84b81b18d51a7
kseggelke918 <katie.seggelke@gmail.com>
2020-08-28 20:07:59 -0600

    start writing CRUD methods for player controller

--- le-pool-backend/app/controllers/players_controller.rb
+++ le-pool-backend/app/controllers/players_controller.rb
@@ -1,16 +1,44 @@
 class PlayersController < ApplicationController
+    # write before action for finding player when refactoring
+
     def index 
+        # do we want to show all players or just the players associated with the game selected...
+        # need to think about the functionality wanted here
     end 
 
     def create 
+        player = Player.create(player_params)
+        if player.save 
+            # render json with serializer
+        else 
+            # render json error
+        end 
     end 
 
     def show 
+        player = Player.find_by(id: params[:id])
+        # render json with serializer
     end 
 
     def update 
+        player = Player.find_by(id: params[:id])
+        player.update(player_params)
+        if player.valid?
+            # render serialized json
+        else 
+            # render json error message
+        end 
     end 
 
     def destroy 
+        player = Player.find_by(id: params[:id])
+        player.destroy
+    end 
+
+    private 
+
+    def player_params
+        # how necessary is this?
+        params.require(:player).permit(:name)
     end 
 end
