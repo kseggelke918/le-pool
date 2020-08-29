ff90ffc28c9139084b9707ef8d5cb4bec762e558
kseggelke918 <katie.seggelke@gmail.com>
2020-08-28 19:54:24 -0600

    stub out controllers

--- le-pool-backend/app/controllers/games_controller.rb
+++ le-pool-backend/app/controllers/games_controller.rb
@@ -1,2 +1,26 @@
 class GamesController < ApplicationController
+    before_action :set_user
+
+    def index 
+        
+    end 
+
+    def create 
+    end 
+
+    def show 
+    end 
+
+    def update 
+    end 
+
+    def destroy 
+    end 
+
+    private 
+
+    def set_user
+        @user = User.find(params[:user_id])
+    end 
+
 end
