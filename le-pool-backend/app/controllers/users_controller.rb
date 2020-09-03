d0c85394b9d717c04765aeb7b7f173e7eb19ef0e
kseggelke918 <katie.seggelke@gmail.com>
2020-09-02 20:19:37 -0600

    write show, new, set_user and user_params in users controller

--- le-pool-backend/app/controllers/users_controller.rb
+++ le-pool-backend/app/controllers/users_controller.rb
@@ -1,13 +1,35 @@
 class UsersController < ApplicationController
+    before_action :set_user
+    
     def create 
+        @user = User.new(user_params)
+        if @user.save 
+            # set session?? session[:user_id] = @user.id
+            render json: @user 
+        else 
+            render json: {error: 'Error creating user'}
+        end 
     end 
 
     def show 
+        render json: @user
     end 
 
     def update 
+        render json: {message: 'this is the update'}
     end 
 
     def destroy 
     end 
+
+    private 
+
+    def set_user
+        @user = User.find_by(id: params[:id])
+    end 
+
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end 
+
 end
