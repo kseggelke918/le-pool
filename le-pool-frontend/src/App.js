9a30693fd1a0f66e4da17e7d561b979c150a4091
kseggelke918 <katie.seggelke@gmail.com>
2020-09-14 14:10:47 -0600

    update date established, typo and create games component file

--- le-pool-frontend/src/App.js
+++ le-pool-frontend/src/App.js
@@ -6,9 +6,9 @@ class App extends Component {
     return (
       <div className='welcome_page_header'>
         <h1>Le Pool</h1>
-        <h3>Established: ####</h3>
+        <h3>Established: 2009</h3>
         <h3>Stillwater, MN</h3>
-        <p>Click here to view all games (goes to games component</p>
+        <p>Click here to view all games (goes to games component)</p>
         <p>Click here to go to all players (goes to players component)</p>
         <p>Click here to sign up</p>
         <p>Login: enter email</p>
