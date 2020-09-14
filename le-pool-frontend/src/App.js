553c56c70c2a842edf793b3e36cb65c74b9d9281
kseggelke918 <katie.seggelke@gmail.com>
2020-09-14 14:37:38 -0600

    install react-router

--- le-pool-frontend/src/App.js
+++ le-pool-frontend/src/App.js
@@ -4,11 +4,13 @@ import Games from './components/games.js'
 import Players from './components/players.js'
 import Signup from './components/signup.js'
 import Login from './components/login.js'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 class App extends Component {
   render () {
     return (
-      <div className='welcome_page_header'>
+      <Router>
+        <div className='welcome_page_header'>
         <h1>Le Pool</h1>
         <h3>Established: 2009</h3>
         <h3>Stillwater, MN</h3>
@@ -17,6 +19,8 @@ class App extends Component {
         <Games />
         <Players />
       </div>
+      </Router>
+      
     )
   }
 }
