2cdb4120d667c46a29dc6fe240f29d05cd7da596
kseggelke918 <katie.seggelke@gmail.com>
2020-10-26 16:13:00 -0600

    stub out sessions controller

--- le-pool-backend/app/controllers/sessions_controller.rb
+++ le-pool-backend/app/controllers/sessions_controller.rb
@@ -1,2 +1,28 @@
 class SessionsController < ApplicationController
+
+    def create 
+        @user = User.find_by(email: params[:session][:email])
+
+        # since no pw, don't need to authenticate?
+        # think about if we want to authenticate...
+
+        # if we authenticate, need to use .authenticate for the password
+        # using if/else
+    end 
+
+    def get_current_user 
+        if logged_in?
+            # render json 
+        else 
+            # render json error
+        end 
+
+    end 
+
+    def destroy 
+        session.clear 
+    end 
+
+
+
 end
