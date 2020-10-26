111f75ab45166f1dfc9f6cb0ecd2b6cfc9382b23
kseggelke918 <katie.seggelke@gmail.com>
2020-10-26 16:05:50 -0600

    change method order in application controller

--- le-pool-backend/app/controllers/application_controller.rb
+++ le-pool-backend/app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
     include Graphiti::Rails
     include Graphiti::Responders
-
-    def logged_in?
-        !!current_user
-    end 
+    # include ::ActionController::Cookies
 
     def current_user 
         User.find_by(id: session[:user_id])
     end 
+    
+    def logged_in?
+        !!current_user
+    end 
+
 end
