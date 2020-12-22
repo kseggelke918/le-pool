614ccd23fceaa0d50a30097599f5c04aae58ae40
kseggelke918 <katie.seggelke@gmail.com>
2020-12-22 13:15:10 -0700

    add rack-cors gem, create componentDidMount, constructor for games.js and getData in games.js

--- le-pool-backend/app/resources/game_resource.rb
+++ le-pool-backend/app/resources/game_resource.rb
@@ -1,6 +1,7 @@
 class GameResource < ApplicationResource
     belongs_to :devise_user
     attribute :devise_user_id, :integer_id
+    attribute :game_name, :string
     many_to_many :players
     
 end
--- le-pool-frontend/src/components/games.js
+++ le-pool-frontend/src/components/games.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Games extends Component {
-    render() {
-        return (
-            <div>
-                This would be all the games
-            </div>
-        )
-    }
+
+  constructor(props){
+    super(props);
+    this.state = {games: []};
+  }
+
+  render() {
+    let games = this.state.games;
+    return <div>This would be all the games  </div>;
+  }
+  
+  async componentDidMount(){
+    console.log("mounted");
+    let data = await getData(`http://localhost:3000/api/games?sort=id`);
+    console.log(data);
+    // set this.state.games = data
+    this.setState({games: data})
+  }
+
 }
 
-export default Games
+
+// putting async in front of the function means that whatever calls me is going to have to wait
+async function getData(url) {
+  // response should be the fetch - await means that we are going to stop and wait for the fetch
+  // await only works if async is in front of the function
+  let response = await fetch(url); 
+  let json = await response.json();
+
+  return json 
+}
+
+export default Games;
