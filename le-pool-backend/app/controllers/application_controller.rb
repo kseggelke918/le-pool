93063bd49da0417d4e24ba6a7f24cef328bcd18d
kseggelke918 <katie.seggelke@gmail.com>
2020-12-22 12:13:07 -0700

    add home route

--- le-pool-backend/app/controllers/application_controller.rb
+++ le-pool-backend/app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
     include Graphiti::Responders
     # include ::ActionController::Cookies
 
+    def home 
+        render plain: "This is the home page"
+    end 
+
+
     def current_user 
         User.find_by(id: session[:user_id])
     end 
--- le-pool-backend/config/routes.rb
+++ le-pool-backend/config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     end 
   
     # your routes go here
+    root 'application#home'
   end  
   # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
